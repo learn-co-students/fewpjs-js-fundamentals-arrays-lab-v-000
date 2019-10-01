d46e1b1561fb8c0ddb809f6751844a15e3e34ffd
Jason Mei <jason.mei403@gmail.com>
2019-10-01 18:38:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = "Milo Otis Garfield".split(" ");
+const prepend = "Milo Otis Garfield".split(" ");
+const removeLast = "Milo Otis Garfield".split(" ");
+const removeFirst = "Milo Otis Garfield".split(" ");
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
