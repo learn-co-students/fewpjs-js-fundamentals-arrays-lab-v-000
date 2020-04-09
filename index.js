d6adb7dbefc4f29af80f37f69f9ae0b57d055a44
Toni-Ty <Antonique2479@yahoo.com>
2020-04-09 04:13:02 -0500

    add pop method

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ prepend.unshift('Odie');
 
 const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.splice(-1);
+// removeLast.pop(); also works
 
 const removeFirst = ["Milo", "Otis", "Garfield"];
 removeFirst.shift();
+
+
