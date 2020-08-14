a2af38c526d3eedcf7b9b22cc086d57de3673d7c
osxgirl <osxgirl@mac.com>
2020-08-14 10:41:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution here!
+var pets = ["Milo", "Otis", "Garfield"];
+
+const append = ["Milo", "Otis", "Garfield"];
+append.push('Odie');
+
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift('Odie');
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop('Garfield');
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift('Milo');
