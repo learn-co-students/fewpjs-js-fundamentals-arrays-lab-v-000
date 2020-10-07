1ae3607d1347852bc9bfb7a50c3a14ced6c79164
Laura Messa Travin <laura.travin@gmail.com>
2020-10-07 21:56:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const append =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const prepend =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const removeLast =["Milo", "Otis", "Garfield"];
+// Write your solution here!
+const removeFirst =["Milo", "Otis", "Garfield"];
+
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
